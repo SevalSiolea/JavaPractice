de74c430a1836ec3a1632d06abd9b547f3b20be2
Siolea <siolea@qq.com>
2024-07-13 11:58:34 +0800

    Add template of input and output.

--- src/Codeforces/Template.java
+++ src/Codeforces/Template.java
@@ -1,31 +1,46 @@
 package Codeforces;
 
+import java.util.Scanner;
+
 public class Template {
 
     private enum solution { solution1, solution2, solution3 }
 
     public static void main( String[] args ) {
-        solution( solution.solution1 );
+
+        Scanner scanner = new Scanner( System.in );
+
+        int testCount = scanner.nextInt();
+        int[] results = new int[ testCount ];
+        for( int i=0; i < testCount; i++ ) {
+            int arrLength = scanner.nextInt();
+            int[] arr = new int[ arrLength ];
+            for( int j=0; j < arrLength; j++ )
+                arr[ j ] = scanner.nextInt();
+            results[ i ] = solution( solution.solution1 );
+        }
+
+        for( int i=0; i < testCount; i++ )
+            System.out.print( results[ i ] );
     }
 
-    private static void solution( solution solution ) {
+    private static int solution( solution solution ) {
         switch( solution ) {
             case solution1:
-                solution1();
-                break;
+                return solution1();
             case solution2:
-                solution2();
-                break;
+                return solution2();
             case solution3:
-                solution3();
-                break;
+                return solution3();
+            default:
+                return -1;
         }
     }
 
-    private static void solution1() {}
+    private static int solution1() { return 0; }
 
-    private static void solution2() {}
+    private static int solution2() { return 0; }
 
-    private static void solution3() {}
+    private static int solution3() { return 0; }
 
 }
