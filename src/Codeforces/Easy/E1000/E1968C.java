fa1affd6c83a4fc027bf04b50e386314ea35f2c6
Siolea <siolea@qq.com>
2024-07-22 11:01:20 +0800

    Easiest solution of E1968C.

---
+++ src/Codeforces/Easy/E1000/E1968C.java
@@ -0,0 +1,58 @@
+package Codeforces.Easy.E1000;
+
+import java.util.Scanner;
+
+public class E1968C {
+
+    private enum Strategy { strategy1 }
+
+    public static void main( String[] args ) {
+
+        Scanner scanner = new Scanner( System.in );
+        Strategy strategy = Strategy.strategy1;
+
+        int testCount = scanner.nextInt();
+        int[][] results = new int[ testCount ][];
+        for( int i=0; i < testCount; i++ ) {
+            int arrLength = scanner.nextInt();
+            int[] arr = new int[ arrLength ];
+            arr[ arrLength - 1 ] = 0;
+            for( int j=0; j < arrLength - 1; j++ )
+                arr[ j ] = scanner.nextInt();
+            results[ i ] = solution( strategy, arr );
+        }
+
+        for( int i=0; i < testCount; i++ ) {
+            for( int num : results[ i ] )
+                System.out.print( num + " " );
+            System.out.println();
+        }
+
+    }
+
+    private static int[] solution( Strategy strategy, int[] arr ) {
+        switch( strategy ) {
+            case strategy1:
+                return solution1( arr );
+            default:
+                return new int[] {};
+        }
+    }
+
+    // easiest solution
+    // result[ i ] = arr[ i-1 ] + result[ i-1 ] * k
+    // k satisfy result[ i ] > arr[ i ]
+    private static int[] solution1( int[] arr ) {
+
+        int[] result = new int[ arr.length ];
+        result[ 0 ] = arr[ 0 ] + 1;
+        for( int i=1; i < arr.length; i++ ) {
+            result[ i ] = arr[ i-1 ];
+            while( result[ i ] <= arr[ i ] )
+                result[ i ] += result[ i-1 ];
+        }
+
+        return result;
+    }
+
+}
