cb42c4f055a1596db648ba358c0fe29b79f4c1d7
Siolea <siolea@qq.com>
2024-07-22 10:27:43 +0800

    Easiest solution of E1975B.

---
+++ src/Codeforces/Easy/E1000/E1975B.java
@@ -0,0 +1,50 @@
+package Codeforces.Easy.E1000;
+
+import java.util.Scanner;
+
+public class E1975B {
+
+    private enum Strategy { strategy1 }
+
+    public static void main( String[] args ) {
+
+        Scanner scanner = new Scanner( System.in );
+        Strategy strategy = Strategy.strategy1;
+
+        int testCount = scanner.nextInt();
+        boolean[] results = new boolean[ testCount ];
+        for( int i=0; i < testCount; i++ ) {
+            int arrLength = scanner.nextInt();
+            int[] arr = new int[ arrLength ];
+            for( int j=0; j < arrLength; j++ )
+                arr[ j ] = scanner.nextInt();
+            results[ i ] = solution( strategy, arr );
+        }
+
+        for( int i=0; i < testCount; i++ )
+            System.out.println( results[ i ] ? "YES" : "NO" );
+    }
+
+    private static boolean solution( Strategy strategy, int[] arr ) {
+        switch( strategy ) {
+            case strategy1:
+                return solution1( arr );
+            default:
+                return false;
+        }
+    }
+
+    // easiest solution by traversing array
+    // examine whether they can all be divided by smallest two elements
+    private static boolean solution1( int[] arr ) {
+
+        java.util.Arrays.sort( arr );
+
+        for( int i=0; i < arr.length; i++ )
+            if( arr[ i ] % arr[ 0 ] != 0 && arr[ i ] % arr[ 1 ] != 0 )
+                return false;
+
+        return true;
+    }
+
+}
