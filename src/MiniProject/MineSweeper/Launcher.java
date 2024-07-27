09bc63a0d61449d0388c5c6d2ff983798164ccd7
Siolea <siolea@qq.com>
2024-07-27 12:51:55 +0800

    Git test done.

--- src/MiniProject/MineSweeper/Launcher.java
+++ src/MiniProject/MineSweeper/Launcher.java
@@ -20,7 +20,6 @@ public class Launcher {
     /**================================ private method ================================**/
 
 
-
     private static boolean startGame() {
 
         java.util.Scanner scanner = new java.util.Scanner( System.in );
