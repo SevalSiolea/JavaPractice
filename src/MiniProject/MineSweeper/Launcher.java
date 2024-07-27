935b34330eba97b050d3481a665146f0b61320bf
Siolea <siolea@qq.com>
2024-07-27 11:29:39 +0800

    Redesign methods and recode input/output of Launcher.

--- src/MiniProject/MineSweeper/Launcher.java
+++ src/MiniProject/MineSweeper/Launcher.java
@@ -8,48 +8,66 @@ public class Launcher {
 
     public static void main( String[] args ) {
 
-        java.util.Scanner scanner = new java.util.Scanner( System.in );
-
-        while( true ) {
-            System.out.print( "Start a new game ( yes / no ) : " );
-            String startGame = scanner.next();
-            if( startGame.equals( "no" ) )
-                break;
-            else if( !startGame.equals( "yes" ) )
-                throw new RuntimeException( "Please enter right input!" );
-
-            System.out.println( "Select a difficulty ( easy / medium / hard / customized ) : easy / medium / hard / customized : " );
-            String difficulty = scanner.next().toUpperCase();
-            difficulty = difficulty.equals( "E" ) ? "EASY" : difficulty;
-            difficulty = difficulty.equals( "M" ) ? "MEDIUM" : difficulty;
-            difficulty = difficulty.equals( "H" ) ? "HARD" : difficulty;
-            difficulty = difficulty.equals( "C" ) ? "CUSTOMIZED" : difficulty;
-
-            Game game;
-            if( difficulty.equals( "EASY" ) || difficulty.equals( "MEDIUM" ) || difficulty.equals( "HARD" ) )
-                game = new Game( Difficulty.valueOf( difficulty ) );
-            else if( difficulty.equals( "CUSTOMIZED" ) ) {
-                System.out.print( "Set row of game ( from 1 to 64 ) : " );
-                int row = scanner.nextInt();
-                if( row < 1 || row > 64 )
-                    throw new RuntimeException( "Please enter right input!" );
-
-                System.out.print( "Set column of game ( from 1 to 64 ) : " );
-                int column = scanner.nextInt();
-                if( column < 1 || column > 64 )
-                    throw new RuntimeException( "Please enter right input!" );
-
-                System.out.print( "Set number of mines of game ( from 1 to row * col ) : " );
-                int mineCount = scanner.nextInt();
-                if( mineCount < 1 || mineCount > row * column )
-                    throw new RuntimeException( "Please enter right input!" );
-
-                game = new Game( Difficulty.CUSTOMIZED, row, column, mineCount );
-            } else
-                throw new RuntimeException( "Please enter right input!" );
-
+        while( startGame() ) {
+            Difficulty difficulty = getDifficulty();
+            Game game = initGame( difficulty );
             game.playGame();
         }
+
+    }
+
+
+    /**================================ private method ================================**/
+
+
+    private static  boolean startGame() {
+
+        java.util.Scanner scanner = new java.util.Scanner( System.in );
+
+        System.out.print( "Start a new game ( yes / no ) : " );
+        String startGame = scanner.next().toLowerCase();
+        if( startGame.equals( "no" ) )
+            return false;
+        else if( startGame.equals( "yes" ) )
+            return true;
+        else
+            throw new RuntimeException( "Please enter right input!" );
+
+    }
+
+    private static Difficulty getDifficulty() {
+
+        java.util.Scanner scanner = new java.util.Scanner( System.in );
+
+        System.out.print( "Select a difficulty ( easy / medium / hard / customized ) : " );
+        String difficultyStr = scanner.next().toUpperCase();
+
+        if( difficultyStr.equals( "E" ) ) return Difficulty.EASY;
+        if( difficultyStr.equals( "M" ) ) return Difficulty.MEDIUM;
+        if( difficultyStr.equals( "H" ) ) return Difficulty.HARD;
+        if( difficultyStr.equals( "C" ) ) return Difficulty.CUSTOMIZED;
+
+        return Difficulty.valueOf( difficultyStr );
+
+    }
+
+    private static Game initGame( Difficulty difficulty ) {
+
+        java.util.Scanner scanner = new java.util.Scanner( System.in );
+
+        if( difficulty == Difficulty.EASY || difficulty == Difficulty.MEDIUM || difficulty == Difficulty.HARD )
+            return new Game( difficulty );
+        else if( difficulty != Difficulty.CUSTOMIZED )
+            throw new RuntimeException( "Please enter right input!" );
+
+        System.out.print( "Set row of the game ( from 1 to 32 ) : " );
+        int row = scanner.nextInt();
+        System.out.print( "Set col of the game ( from 1 to 32 ) : " );
+        int col = scanner.nextInt();
+        System.out.print( "Set number of mines ( from 1 to row * col ) " );
+        int mineCount = scanner.nextInt();
+
+        return new Game( Difficulty.CUSTOMIZED, row, col, mineCount );
 
     }
 
