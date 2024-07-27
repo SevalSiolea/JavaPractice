1e1beb8cdf6487b7badee139d083ae4ba593d7a1
Siolea <siolea@qq.com>
2024-07-27 20:19:34 +0800

    Add comments of E1977B.

--- src/Codeforces/Easy/E1100/E1977B.java
+++ src/Codeforces/Easy/E1100/E1977B.java
@@ -1,5 +1,14 @@
 package Codeforces.Easy.E1100;
 
+/**
+ * Difficulty : E1100-difficult<br>
+ * Algorithm : constructive, math<br>
+ * Feature : use 4-base to constructive a clever result<br>
+ * Solution : solution1<br>
+ * Date : 2024.7.25<br>
+ *
+ * Solution1 : use 4-base to constructive a clever result<br>
+ */
 public class E1977B {
 
     private enum Strategy { STRATEGY1 }
@@ -31,33 +40,64 @@ public class E1977B {
         }
     }
 
+    /**
+     * Description : clever solution, constructive solution<br>
+     * Complexity : timeO( logN ), space O( 1 )<br>
+     * Hint : use 4-base to denote num and sum of array<br>
+     * Feature : use 4-base to construct a clever result<br>
+     *
+     * Idea : consider a pair of neighboring binary digit a[ i ]a[ i+1 ]<br>
+     * Idea : denote it as b in num or c in array<br>
+     * Idea : in fact, b or c is a 4-base digit<br>
+     * Idea : in num, a[ i ]a[ i+1 ] may be 00, 01, 10, 11<br>
+     * Idea : so b may be 0, 1, 2, 3<br>
+     * Idea : in array, a[ i ]a[ i+1 ] may be -1_0, 0_-1, 00, 01, 10<br>
+     * Idea : so c may -2, -1, 0, 1, 2<br>
+     *
+     * Idea : consider how to denote num by sum of array<br>
+     * Idea : firstly set c = b, if b is 0, 1, 2, it is possible<br>
+     * Idea : if b is 3, set c = 3 = 4 - 1, thus set c = -1 and generate a carry<br>
+     * Idea : if b is 3, and c get a carry, c = 3 + 1 = 4, thus set c = 0 and generate a carry<br>
+     * Idea : after iterating over 4-base digits of num, we construct an array<br>
+     *
+     * Idea : however, this array maybe break rule 4<br>
+     * Idea : which happens when c[ i ] is -1, 1 and c[ i+1 ] is 2<br>
+     * Idea : to modify result, set c[ i+1 ] = 4 - 2, then c[ i+1 ] = -2 and generate a carry<br>
+     * Idea : therefore c[ i ] is 0, 2 and c[ i+1 ] is -2, which does not break rule 4<br>
+     *
+     * @param num given num
+     * @return result
+     */
     private static int[] solution1( int num ) {
 
-        int[] numCos = new int[ 16 ];
+        // get 4-base digits of num
+        int[] numDigits = new int[ 16 ];
         for( int i = 15; i >= 0; i-- ) {
-            numCos[ i ] = num % 4;
+            numDigits[ i ] = num % 4;
             num >>= 2;
         }
 
+        // core code of getting correct 4-base result
         for( int i = 15; i > 0; i-- ) {
-            if ( numCos[ i ] == 2 && ( numCos[ i - 1 ] == -1 || numCos[ i - 1 ] == 1 || numCos[ i - 1 ] == 3 ) ) {
-                numCos[ i ] = -2;
-                numCos[ i - 1 ] += 1;
+            if ( numDigits[ i ] == 2 && ( numDigits[ i - 1 ] == -1 || numDigits[ i - 1 ] == 1 || numDigits[ i - 1 ] == 3 ) ) {
+                numDigits[ i ] = -2;
+                numDigits[ i - 1 ] += 1;
             }
-            if ( numCos[ i ] == 3 ) {
-                numCos[ i ] = -1;
-                numCos[ i - 1 ] += 1;
+            if ( numDigits[ i ] == 3 ) {
+                numDigits[ i ] = -1;
+                numDigits[ i - 1 ] += 1;
             }
-            if( numCos[ i ] == 4 ) {
-                numCos[ i ] = 0;
-                numCos[ i - 1 ] += 1;
+            if( numDigits[ i ] == 4 ) {
+                numDigits[ i ] = 0;
+                numDigits[ i - 1 ] += 1;
             }
         }
 
+        // getting 2-base result by 4-base result
         int[] bitNumCos = new int[ 32 ];
         java.util.Arrays.fill( bitNumCos, 0 );
-        for( int i = 0; i < numCos.length; i++ )
-            switch( numCos[ i ] ) {
+        for( int i = 0; i < numDigits.length; i++ )
+            switch( numDigits[ i ] ) {
                 case -2:
                     bitNumCos[ 2 * i ] = -1;
                     bitNumCos[ 2 * i + 1 ] = 0;
