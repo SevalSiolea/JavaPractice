700ee4ab1562fef66ac39a965baddea2698bdcf0
Siolea <siolea@qq.com>
2024-08-01 08:10:23 +0800

    Little modify.

--- src/Codeforces/Template.java
+++ src/Codeforces/Template.java
@@ -7,9 +7,9 @@ package Codeforces;
  * package difficulty : Easy(E), Medium(M), Hard(H), Expert(EX), Master(MA)<br>
  * codeforces difficulty : E1000, E1100, E1200, M1300, M1400, M1500, H1600, H1700, H1800<br>
  * codeforces difficulty : EX1900, EX2000, MA2100, MA2200<br>
- * actual difficulty : easy, medium, difficult compared to average codeforces difficulty<br>
- * actual difficulty : for example, E1300-difficult maybe M1400 or M1500<br>
- * for example : H1600-medium, E1200-difficult, EX2000-easy<br>
+ * actual difficulty : easy, medium, hard compared to average codeforces difficulty<br>
+ * actual difficulty : for example, E1300-hard maybe M1400 or M1500<br>
+ * for example : H1600-medium, E1200-hard, EX2000-easy<br>
  *
  * <p>
  * Data structure tag : data structure of this problem<br>
